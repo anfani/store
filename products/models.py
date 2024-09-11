acd009008a3aedcfc65efaaf8e3ab56833857970
anfani <tvink1040@mail.ru>
2024-09-11 16:19:29 +0300

    закончил модуль 3.7

--- products/models.py
+++ products/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
+
 class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
@@ -13,5 +18,4 @@ class Product(models.Model):
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'Продукт: {self.name} | Категория: {self.description.name}'
-
+        return f'Продукт: {self.name} | Категория: {self.category}'
