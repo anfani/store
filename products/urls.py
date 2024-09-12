aa41327dd5be156d4587d75d35d4e2c51d1d986f
anfani <tvink1040@mail.ru>
2024-09-12 17:25:43 +0300

    добавил декоратор login req закончил модуль 6

--- products/urls.py
+++ products/urls.py
@@ -6,6 +6,8 @@ app_name = 'products'
 
 urlpatterns = [
     path('', products, name='index'),
+    path('category/<int:category_id>', products, name='category'),
+    path('page/<int:page_number>', products, name='paginator'),
     path('baskets/add/<int:product_id>/', baskets_add, name='basket_add'),
     path('baskets/remove/<int:basket_id>/', baskets_remove, name='baskets_remove'),
 ]
