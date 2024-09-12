00c01d8adae484cd0ce6dcf18556f35bcd5a7b84
anfani <tvink1040@mail.ru>
2024-09-12 22:05:02 +0300

    закончил модуль 7.6

--- products/urls.py
+++ products/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 
-from products.views import products, baskets_add, baskets_remove
+from products.views import ProductListView, baskets_add, baskets_remove
 
 app_name = 'products'
 
 urlpatterns = [
-    path('', products, name='index'),
-    path('category/<int:category_id>', products, name='category'),
-    path('page/<int:page_number>', products, name='paginator'),
+    path('', ProductListView.as_view(), name='index'),
+    path('category/<int:category_id>', ProductListView.as_view(), name='category'),
     path('baskets/add/<int:product_id>/', baskets_add, name='basket_add'),
     path('baskets/remove/<int:basket_id>/', baskets_remove, name='baskets_remove'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,30 +1,37 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 
 from products.models import Product, ProductCategory, Basket
-from django.core.paginator import Paginator
-
-from users.models import User
-
-# Create your views here.
-def index(request):
-    context = {'title': 'Test Title'}
-    return render(request, 'products/index.html', context)
 
 
-def products(request, category_id=None, page_number=1):
-    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
 
-    per_page = 3
-    paginator = Paginator(products, per_page)
-    products_paginator = paginator.page(page_number)
+class IndexView(TemplateView):
+    template_name = 'products/index.html'
 
-    context = {
-        'title': 'Store- Каталог',
-        'categories': ProductCategory.objects.all(),
-        'products': products_paginator,
-    }
-    return render(request, 'products/products.html', context)
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data()
+        context['title'] = 'Store'
+        return context
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/products.html'
+    paginate_by = 3
+
+    def get_queryset(self):
+        queryset = super(ProductListView, self).get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id) if category_id else queryset
+
+    def get_context_data(self, object_list=None, **kwargs):
+        context = super(ProductListView, self).get_context_data()
+        context['title'] = 'Store- Каталог'
+        context['categories'] = ProductCategory.objects.all()
+        return context
+
 
 @login_required
 def baskets_add(request, product_id):
